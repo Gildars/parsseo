c8118ce6733cae9e973ca7c54231db02464fe1ad
Gildars <thegilldars@gmail.com>
2021-03-21 21:05:20 +0000

    Added getLinksCartridgesAndInk()

--- parse.php
+++ parse.php
@@ -2,41 +2,87 @@
 require_once "./vendor/autoload.php";
 
 use DiDom\Document;
+use DiDom\Element;
 
-class ParserPrinters {
-
-    private $lang; 
-
-public function __construct($lang = 'ua')
+class ParserPrinters
 {
-    $this->lang = $lang;
-    $this->parse();
-}
 
-protected function parse(){
-  $linksTypesPrinters = $this->getBrandsLink();
-  $this->getPrintersNames($linksTypesPrinters);
-}
+    private $lang;
 
-protected function getBrandsLink()
-{
-    $document = new Document(`https://prote.ua/{$this->lang}/brands/`, true);
-    $printType = [
-        'urk' => ['лазерні', 'матирчні','струменеві'], 
-        'rus' => ['лазерные', 'матричные', 'струйные']
-    ];
+    private $printers;
 
-  return $document->find('a[title^=лазерні]');
-}
-
-protected function getPrintersNames(array $links){
-    foreach ($links as $link) {
-        $document = new Document($link->attr('href'), true);
-        $printers = $document->find('.plist-list ul li');
-        var_dump($printers);
-
+    public function __construct($lang = 'ua')
+    {
+        $this->lang = $lang;
+        $this->parse();
     }
-}
+
+    private function parse()
+    {
+        $linksTypesPrinters = $this->getBrandsLinks();
+        $this->getPrintersWithLinkMaterials($linksTypesPrinters);
+        $this->getLinksCartridgesAndInk();
+    }
+
+    private function getBrandsLinks()
+    {
+        $document = new Document('https://prote.ua/ua/brands/', true);
+        $printType = [
+            'ua' => ['лазерні', 'матирчні', 'струменеві'],
+            'ru' => ['лазерные', 'матричные', 'струйные']
+        ];
+        $findLinks = [];
+        foreach ($printType[$this->lang] as $type) {
+            $query = 'a[title^=' . $type . ']';
+
+            $query = $document->find($query);
+            if (!empty($query)) {
+                $findLinks[] = $query;
+            }
+        }
+
+        $links = [];
+        foreach ($findLinks as $link) {
+            foreach ($link as $domElement) {
+                $links[] = $domElement->attr('href');
+            }
+        }
+        return $links;
+    }
+
+    private function getPrintersWithLinkMaterials(array $links)
+    {
+        $printersLinks = [];
+        foreach ($links as $link) {
+            $document = new Document($link, true);
+            $printersLinks[] = $document->find('.plist-list a');
+        };
+
+        foreach ($printersLinks as $link) {
+            foreach ($link as $domElement) {
+                $this->printers[] = [
+                    'title' => $domElement->text(),
+                    'materials' => $domElement->attr('href')
+                ];
+            }
+        }
+        // var_dump($this->printers);
+    }
+    private function getLinksCartridgesAndInk()
+    {
+        $types = [
+            'ua' => ['картриджі', 'чорнила'],
+            'ru' => ['картриджи', 'чернила']
+        ];
+        foreach ($this->printers as $key => $printer) {
+            print_r($printer);
+            $document = new Document($printer->materials, true);
+            foreach ($types as $type) {
+                $link = $document->first('.cats ' + 'a[title^=' . $type . ']');
+                $this->printers[$key][$type] = $document->find('.cats ' + 'a[title^=' . $type . ']');
+            }
+        }
+    }
 }
 
 $parserPrinters = new ParserPrinters('ua');
